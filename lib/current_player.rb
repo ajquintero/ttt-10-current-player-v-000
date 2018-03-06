fe8288a4845611ee5ca472d29bbee22ed2c39639
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-06 18:42:01 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,27 @@
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+  count = 0
   def turn_count(board)
-    count = 0
+    
     board.each do |turn|
       puts "There have been #{turn} turns played"
       count += 1
     end
   end
 
-def current_player(board)
-  if turn_count(board) == 0 || turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
-end
+
+
+
+
+
+
+
+
+
+
+# def current_player(board)
+#   if turn_count(board) == 0 || turn_count(board) % 2 == 0
+#     return "X"
+#   else
+#     return "O"
+#   end
+# end
