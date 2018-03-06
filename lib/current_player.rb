32fed2324eeb0b9ff3c2411559bb3295bc9808e8
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-06 18:59:53 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,15 +9,13 @@
     end
   end
 
-
-
-
-
-
-
-
-
-
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
 
 # def current_player(board)
 #   if turn_count(board) == 0 || turn_count(board) % 2 == 0
