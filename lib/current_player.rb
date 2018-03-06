0c4bb17ae98f3b72384cd2bcbd4f4313d053a011
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-06 19:57:47 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@
   def turn_count(board)
     count = 0
     board.each do |turn|
-      if turn = "X" || turn = "O"
+      if turn == "X" || turn == "O"
       puts "#{turn}"
       count += 1
       end
